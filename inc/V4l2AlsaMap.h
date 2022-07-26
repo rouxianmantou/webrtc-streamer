bef5c094d71fb4eb9caf26154c88d53e01f9070a
guo.hanlin <guo.hanlin@chinaott.net>
2022-07-26 13:43:56 +0800

    Remove some github workflow.

--- inc/V4l2AlsaMap.h
+++ inc/V4l2AlsaMap.h
@@ -15,45 +15,54 @@
 /* ---------------------------------------------------------------------------
 **  get a "deviceid" from uevent sys file
 ** -------------------------------------------------------------------------*/
-std::string getDeviceId(const std::string& evt) {
-    std::string deviceid;
-    std::istringstream f(evt);
-    std::string key;
-    while (getline(f, key, '=')) {
-            std::string value;
-	    if (getline(f, value)) {
-		    if ( (key =="PRODUCT") || (key == "PCI_SUBSYS_ID") ) {
-			    deviceid = value;
-			    break;
-		    }
-	    }
-    }
-    return deviceid;
+std::string getDeviceId(const std::string &evt)
+{
+	std::string deviceid;
+	std::istringstream f(evt);
+	std::string key;
+	while (getline(f, key, '='))
+	{
+		std::string value;
+		if (getline(f, value))
+		{
+			if ((key == "PRODUCT") || (key == "PCI_SUBSYS_ID"))
+			{
+				deviceid = value;
+				break;
+			}
+		}
+	}
+	return deviceid;
 }
-std::map<std::string,std::string> getVideoDevices() {
-	std::map<std::string,std::string> videodevices;
+std::map<std::string, std::string> getVideoDevices()
+{
+	std::map<std::string, std::string> videodevices;
 	std::string video4linuxPath("/sys/class/video4linux");
 	DIR *dp = opendir(video4linuxPath.c_str());
-	if (dp != NULL) {
+	if (dp != NULL)
+	{
 		struct dirent *entry = NULL;
-		while((entry = readdir(dp))) {
+		while ((entry = readdir(dp)))
+		{
 			std::string devicename;
 			std::string deviceid;
-			if (strstr(entry->d_name,"video") == entry->d_name) {
+			if (strstr(entry->d_name, "video") == entry->d_name)
+			{
 				std::string devicePath(video4linuxPath);
 				devicePath.append("/").append(entry->d_name).append("/name");
 				std::ifstream ifsn(devicePath.c_str());
 				devicename = std::string(std::istreambuf_iterator<char>{ifsn}, {});
-				devicename.erase(devicename.find_last_not_of("\n")+1);
+				devicename.erase(devicename.find_last_not_of("\n") + 1);
 
 				std::string ueventPath(video4linuxPath);
 				ueventPath.append("/").append(entry->d_name).append("/device/uevent");
 				std::ifstream ifsd(ueventPath.c_str());
 				deviceid = std::string(std::istreambuf_iterator<char>{ifsd}, {});
-				deviceid.erase(deviceid.find_last_not_of("\n")+1);
+				deviceid.erase(deviceid.find_last_not_of("\n") + 1);
 			}
 
-			if (!devicename.empty() && !deviceid.empty()) {
+			if (!devicename.empty() && !deviceid.empty())
+			{
 				videodevices[devicename] = getDeviceId(deviceid);
 			}
 		}
@@ -62,26 +71,32 @@ std::map<std::string,std::string> getVideoDevices() {
 	return videodevices;
 }
 
-std::map<std::string,std::string> getAudioDevices() {
-	std::map<std::string,std::string> audiodevices;
+std::map<std::string, std::string> getAudioDevices()
+{
+	std::map<std::string, std::string> audiodevices;
 	std::string audioLinuxPath("/sys/class/sound");
 	DIR *dp = opendir(audioLinuxPath.c_str());
-	if (dp != NULL) {
+	if (dp != NULL)
+	{
 		struct dirent *entry = NULL;
-		while((entry = readdir(dp))) {
+		while ((entry = readdir(dp)))
+		{
 			std::string devicename;
 			std::string deviceid;
-			if (strstr(entry->d_name,"card") == entry->d_name) {
+			if (strstr(entry->d_name, "card") == entry->d_name)
+			{
 				std::string devicePath(audioLinuxPath);
-				devicePath.append("/").append(entry->d_name).append("/device/uevent");	
+				devicePath.append("/").append(entry->d_name).append("/device/uevent");
 
 				std::ifstream ifs(devicePath.c_str());
 				std::string deviceid = std::string(std::istreambuf_iterator<char>{ifs}, {});
-				deviceid.erase(deviceid.find_last_not_of("\n")+1);
+				deviceid.erase(deviceid.find_last_not_of("\n") + 1);
 				deviceid = getDeviceId(deviceid);
 
-				if (!deviceid.empty()) {
-					if (audiodevices.find(deviceid) == audiodevices.end()) {
+				if (!deviceid.empty())
+				{
+					if (audiodevices.find(deviceid) == audiodevices.end())
+					{
 						std::string audioname(entry->d_name);
 						int deviceNumber = atoi(audioname.substr(strlen("card")).c_str());
 
@@ -89,30 +104,45 @@ std::map<std::string,std::string> getAudioDevices() {
 						devname += std::to_string(deviceNumber);
 						audiodevices[deviceid] = devname;
 					}
-				}							
+				}
 			}
-		}	
-		closedir(dp);	
+		}
+		closedir(dp);
 	}
 	return audiodevices;
 }
 
-std::map<std::string,std::string>  getV4l2AlsaMap() {
-	std::map<std::string,std::string> videoaudiomap;
+std::map<std::string, std::string> getV4l2AlsaMap()
+{
+	std::map<std::string, std::string> videoaudiomap;
 
-	std::map<std::string,std::string> videodevices = getVideoDevices();
-	std::map<std::string,std::string> audiodevices = getAudioDevices();
+	std::map<std::string, std::string> videodevices = getVideoDevices();
+	std::map<std::string, std::string> audiodevices = getAudioDevices();
 
-	for (auto & id : videodevices) {
+	std::cout << "****** ****** ****** ****** ****** ****** v_device" << std::endl;
+	for (auto &v_device : videodevices)
+	{
+		std::cout << "GHL-TEST: " << v_device.first << ":" << v_device.second << std::endl;
+	}
+	std::cout << "****** ****** ****** ****** ****** ****** a_device" << std::endl;
+	for (auto &a_device : audiodevices)
+	{
+		std::cout << "GHL-TEST: " << a_device.first << ":" << a_device.second << std::endl;
+	}
+	std::cout << "****** ****** ****** ****** ****** ****** end" << std::endl;
+
+	for (auto &id : videodevices)
+	{
 		auto audioDevice = audiodevices.find(id.second);
-		if (audioDevice != audiodevices.end()) {
-			std::cout <<  id.first << "=>" << audioDevice->second << std::endl;
+		if (audioDevice != audiodevices.end())
+		{
+			std::cout << id.first << "=>" << audioDevice->second << std::endl;
 			videoaudiomap[id.first] = audioDevice->second;
 		}
 	}
-	
+
 	return videoaudiomap;
 }
 #else
-std::map<std::string,std::string>  getV4l2AlsaMap() { return std::map<std::string,std::string>(); };
+std::map<std::string, std::string> getV4l2AlsaMap() { return std::map<std::string, std::string>(); };
 #endif
